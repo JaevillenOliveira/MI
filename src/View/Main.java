1768cf08d91d4ba4eec95cda81e7dc4befbe3b94
Almir Neto <almirneto762@hotmail.com>
2018-02-05 19:50:31 -0300

    Add files via upload

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,12 +1,13 @@
+
 package View;
-import Controller.Controller;
+
 import Exceptions.*;
+import Model.*;
 import Facade.Facade;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.List;
 import javafx.application.Application;
 import javafx.event.*;
 import javafx.geometry.*;
@@ -14,7 +15,6 @@ import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -28,15 +28,21 @@ import javafx.stage.Stage;
  */
 public class Main extends Application{
     
-    private static Facade facade = new Facade();
+    private static Facade facade;
     private static Scene main;
     private static Scene signUp;
+    private static Scene userScene;
+    private static Scene adminScene;
+    private static Scene addCityScene;
+    private static Scene addRoadScene;
+    private static Scene addPlaceToEatScene;
 
     /**
      * Main method.
      * @param args 
      */
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, UnsupportedEncodingException{
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, UnsupportedEncodingException, DuplicatedDataException{
+        facade = new Facade();
         launch(args);
     }
 
@@ -44,9 +50,14 @@ public class Main extends Application{
     public void start(Stage primaryStage) throws Exception {
         
         drawSignUpScene(primaryStage);
+        drawUserScene(primaryStage);
+        drawAdminScene(primaryStage);
+        drawAddCityScene(primaryStage);
+        
         
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.TOP_CENTER);
+        grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(140, 140, 140, 140));
         
@@ -74,7 +85,35 @@ public class Main extends Application{
         Button signIn = new Button("SignIn");
         signIn.setOnAction((ActionEvent event)->{
             if(cpfGetter.getText().isEmpty() || pwGetter.getText().isEmpty()){
-                alert();
+                alert("There are Empty Fields.");
+            }
+            else{
+                try{
+                    User user = facade.doLogin(cpfGetter.getText(), pwGetter.getText());
+                    if(user == null){
+                        alert("Password incorrect.");
+                        pwGetter.setText("");
+                    }
+                    else if(user.getType() == UserType.USER){
+                        cpfGetter.setText("");
+                        pwGetter.setText("");
+                        primaryStage.setScene(userScene);
+                    }
+                    else{
+                        cpfGetter.setText("");
+                        pwGetter.setText("");
+                        primaryStage.setScene(adminScene);
+                    }
+                } catch (NotFoundException ex) {
+                    alert("There's no User registered with this CPF.");
+                    cpfGetter.setText("");
+                    pwGetter.setText("");
+                } catch (NoSuchAlgorithmException ex) {
+                    
+                } catch (UnsupportedEncodingException ex) {
+                    
+                }
+                
             }
         });
         bp.setCenter(signIn);
@@ -118,6 +157,7 @@ public class Main extends Application{
     }
     
     private void drawSignUpScene(Stage primaryStage){
+        
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.TOP_CENTER);
         grid.setVgap(10);
@@ -165,23 +205,17 @@ public class Main extends Application{
         Button register = new Button("Register");
         register.setOnAction((ActionEvent event)->{
             if(nameGetter.getText().isEmpty() || cpfGetter.getText().isEmpty() || pwGetter.getText().isEmpty() || CpwGetter.getText().isEmpty()){
-                alert();
+                alert("There are Empty Fields.");
             }
             else if(!pwGetter.getText().equals(CpwGetter.getText())){
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setContentText("Confirmation Password doesn't Match.");
-                alert.setHeaderText(null);
-                alert.setTitle("ERROR!");
-                alert.showAndWait();
+                alert("Confirmation Password doesn't Match.");
+                pwGetter.setText("");
+                CpwGetter.setText("");
             }
             else{
                 try{
                     facade.newUser(nameGetter.getText(), pwGetter.getText(), cpfGetter.getText(), emailGetter.getText());
-                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                    alert.setHeaderText(null);
-                    alert.setTitle("Sucess!");
-                    alert.setContentText("Sucessfully Registered");
-                    alert.showAndWait();
+                    alertInfo("Sucessfully Registered");
                     nameGetter.setText("");
                     cpfGetter.setText("");
                     emailGetter.setText("");
@@ -190,11 +224,7 @@ public class Main extends Application{
                     primaryStage.setScene(main);
                 }
                 catch(DuplicatedDataException dde){
-                    Alert alert  = new Alert(Alert.AlertType.INFORMATION);
-                    alert.setHeaderText(null);
-                    alert.setTitle("Error");
-                    alert.setContentText("There's already an User with this CPF.");
-                    alert.showAndWait();
+                    alert("There's already an User with this CPF.");
                     nameGetter.setText("");
                     cpfGetter.setText("");
                     emailGetter.setText("");
@@ -208,6 +238,7 @@ public class Main extends Application{
         bpButton.setLeft(register);
         
         Button cancel = new Button("Cancel");
+        cancel.setTextFill(Color.RED);
         cancel.setOnAction((ActionEvent event)->{
             nameGetter.setText("");
             cpfGetter.setText("");
@@ -216,9 +247,7 @@ public class Main extends Application{
             CpwGetter.setText("");
             primaryStage.setScene(main);
         });
-        cancel.setTextFill(Color.RED);
         bpButton.setRight(cancel);
-        
         grid.add(bpButton, 0, 12, 2, 1);
         
         BorderPane footerPane = new BorderPane();
@@ -228,12 +257,287 @@ public class Main extends Application{
         grid.add(footerPane, 0, 32, 2, 1);
     }
     
-    private void alert(){
+    private void drawUserScene(Stage primaryStage){
+        
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(60, 60, 60, 60));
+        
+        userScene = new Scene(grid, 720, 650);
+    }
+    
+    private void drawAdminScene(Stage primaryStage){
+        
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(60, 60, 60, 60));
+        
+        adminScene = new Scene(grid, 720, 650);
+        
+        Label title = new Label("Menu Admin");
+        title.setFont(Font.font("Comic Sans MS",  50));
+        title.setTextFill(Color.MEDIUMTURQUOISE);
+        grid.add(title, 0, 0, 2, 1);
+        
+        BorderPane bp = new BorderPane();
+        grid.add(bp, 0, 1, 2, 1);
+        
+        FlowPane fp = new FlowPane(Orientation.VERTICAL);
+        fp.setVgap(10);
+        fp.setAlignment(Pos.CENTER);
+        fp.setColumnHalignment(HPos.CENTER);
+        bp.setTop(fp);
+        
+        Button addCity = new Button("AddCity");
+        addCity.setMinWidth(100);
+        addCity.setOnAction((ActionEvent event)->{
+            primaryStage.setScene(addCityScene);
+        });
+        
+        
+        Button addRoad = new Button("AddRoad");
+        addRoad.setMinWidth(100);
+        addRoad.setOnAction((ActionEvent event)->{
+            drawAddRoadScene(primaryStage);
+            primaryStage.setScene(addRoadScene);
+        });
+        
+        Button addPlaceToEat = new Button("Add Place to Eat");
+        addPlaceToEat.setMinWidth(100);
+        addPlaceToEat.setOnAction((ActionEvent event)->{
+            primaryStage.setScene(addPlaceToEatScene);
+        });
+        
+        fp.getChildren().add(addCity);
+        fp.getChildren().add(addRoad);
+        fp.getChildren().add(addPlaceToEat);   
+    }
+    
+    private void drawAddCityScene(Stage primaryStage){
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(60, 60, 60, 60));
+        
+        addCityScene = new Scene(grid, 720, 650);
+        
+        Label title = new Label("Adding City");
+        title.setFont(Font.font("Comic Sans MS",  50));
+        title.setTextFill(Color.MEDIUMTURQUOISE);
+        grid.add(title, 0, 0, 2, 1);
+        
+        Text cityName = new Text("City Name: ");
+        grid.add(cityName, 0 ,2);
+        
+        TextField cityNameGetter = new TextField();
+        grid.add(cityNameGetter, 1, 2);
+        
+        Text cityCode = new Text("City Code: ");
+        grid.add(cityCode, 0, 4);
+        
+        TextField cityCodeGetter = new TextField();
+        grid.add(cityCodeGetter, 1, 4);
+        
+        Text longitude = new Text("Longitude: ");
+        grid.add(longitude, 0, 6);
+        
+        TextField longitudeGetter = new TextField();
+        grid.add(longitudeGetter, 1, 6);
+        
+        Text latitude = new Text("Latitude: ");
+        grid.add(latitude, 0, 8);
+        
+        TextField latitudeGetter = new TextField();
+        grid.add(latitudeGetter, 1, 8);
+        
+        Text population = new Text("Population: ");
+        grid.add(population, 0, 10);
+        
+        TextField popGetter = new TextField();
+        grid.add(popGetter, 1, 10);
+        
+        BorderPane btnPane = new BorderPane();
+        grid.add(btnPane, 0, 12, 2, 1);
+        
+        Button add = new Button("Add");
+        add.setOnAction((ActionEvent event)->{
+            boolean isOk = true;
+            if(cityNameGetter.getText().isEmpty() || cityCodeGetter.getText().isEmpty() || latitudeGetter.getText().isEmpty() || longitudeGetter.getText().isEmpty()){
+                alert("There are Empty Fields.");
+            }
+            else{
+                int code = 0;
+                try{
+                    code = Integer.parseInt(cityCodeGetter.getText());
+                }
+                catch(NumberFormatException nfx){
+                    alert("Type a Number in \"City Code\" Field.");
+                    cityCodeGetter.setText("");
+                    isOk = false;
+                }
+                
+                double latitudeDouble = 0 ;
+                try{
+                    latitudeDouble = Double.parseDouble(latitudeGetter.getText());
+                }
+                catch(NumberFormatException nfx){
+                    alert("Type a Number in \"Latitude\" Field like this: X.XXX");
+                    latitudeGetter.setText("");
+                    isOk = false;
+                }
+                
+                double longitudeDouble = 0;
+                try{
+                    longitudeDouble = Double.parseDouble(longitudeGetter.getText());
+                }
+                catch(NumberFormatException nfx){
+                    alert("Type a Number in \"Longitude\" Field like this: X.XXX");
+                    longitudeGetter.setText("");
+                    isOk = false;
+                }
+                
+                double popDouble = 0;
+                try{
+                    popDouble = Double.parseDouble(popGetter.getText());
+                }
+                catch(NumberFormatException nfx){
+                    alert("Type a Number in \"Population\" Field like this: X.XXX");
+                    popGetter.setText("");
+                    isOk = false;
+                }
+                if(isOk == true){
+                    try{
+                        facade.addCity(cityNameGetter.getText(), latitudeDouble, longitudeDouble, code, popDouble);
+                        alertInfo("Successfully Registered!");
+                        cityNameGetter.setText("");
+                        cityCodeGetter.setText("");
+                        latitudeGetter.setText("");
+                        longitudeGetter.setText("");
+                        popGetter.setText("");
+                        primaryStage.setScene(adminScene);
+                    } catch (DuplicateEntryException ex) {
+                        alert("There's already a City with this Code.");
+                        cityNameGetter.setText("");
+                        cityCodeGetter.setText("");
+                        latitudeGetter.setText("");
+                        longitudeGetter.setText("");
+                        popGetter.setText("");
+                    }
+                }
+            } 
+        });
+        btnPane.setLeft(add);
+        
+        Button cancel = new Button("Cancel");
+        cancel.setTextFill(Color.RED);
+        cancel.setOnAction((ActionEvent event)->{
+            cityNameGetter.setText("");
+            cityCodeGetter.setText("");
+            popGetter.setText("");
+            longitudeGetter.setText("");
+            latitudeGetter.setText("");
+            primaryStage.setScene(adminScene);
+        });
+        btnPane.setRight(cancel);
+    }
+    
+    private void drawAddRoadScene(Stage primaryStage){
+        
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(100, 100, 100, 100));
+        
+        addRoadScene = new Scene(grid, 720, 650);
+        
+        Label title = new Label("Adding Road");
+        title.setFont(Font.font("Comic Sans MS",  50));
+        title.setTextFill(Color.MEDIUMTURQUOISE);
+        grid.add(title, 0, 0, 2, 1);
+        
+        List<City> cities = facade.getCities();
+        
+        ComboBox firstCbox = new ComboBox();
+        ComboBox secondCbox = new ComboBox();
+        
+        for(City city : cities){
+            firstCbox.getItems().add(city.getName());
+        }
+        
+        for(City city : cities){
+            secondCbox.getItems().add(city.getName());
+        }
+
+        Text roadSource = new Text("CityCode that Starts: ");
+        grid.add(roadSource, 0, 2);
+        
+        grid.add(firstCbox, 1, 2);
+        
+        Text roadDestiny = new Text("CityCode  that Ends: ");
+        grid.add(roadDestiny, 0, 4);    
+        
+        grid.add(secondCbox, 1, 4);
+        
+        Text roadSize = new Text("Length in KM: ");
+        grid.add(roadSize, 0, 6);
+        
+        TextField roadSizeGetter = new TextField();
+        grid.add(roadSizeGetter, 1, 6);
+        
+        BorderPane btnBP= new BorderPane();
+        grid.add(btnBP, 0, 8, 2, 1);
+        
+        Button add = new Button("Add");
+        add.setOnAction((ActionEvent event)->{
+            boolean isOk = true;
+            City cityA = (City)firstCbox.getSelectionModel().getSelectedItem();
+            City cityB = (City)secondCbox.getSelectionModel().getSelectedItem();
+            if(cityA.getCode() == cityB.getCode()){
+                isOk = false;
+                alert("You can't add a Road that Starts and Ends at the Same City.");
+            }
+            double sizeDouble = 0;
+            try{
+                sizeDouble = Double.parseDouble(roadSizeGetter.getText());
+            }
+            catch(NumberFormatException nfx){
+                isOk = false;
+                alert("Type a Number in \"Road Lenght\" like this: X.XXX");
+                roadSizeGetter.setText("");
+            }
+                        
+        });
+        Button cancel = new Button("Cancel");
+        cancel.setOnAction((ActionEvent event)->{
+            primaryStage.setScene(adminScene);
+            roadSizeGetter.setText("");
+        });
+        cancel.setTextFill(Color.RED);
+        
+        btnBP.setLeft(add);
+        btnBP.setRight(cancel); 
+    }
+    
+    private void alert(String message){
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setHeaderText(null);
         alert.setTitle("Error!");
-        alert.setContentText("There are Empty Fields.");
-        alert.show();
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+    
+    private void alertInfo(String message){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("Info!");
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
 }
